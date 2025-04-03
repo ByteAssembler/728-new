f5ad65e01ed85f92cfc2ef3a366beec0a69511fc
ByteAssembler <65082118+ByteAssembler@users.noreply.github.com>
2025-04-03 11:12:57 +0200

    feat: update page title and remove unnecessary links in Home component

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -17,13 +17,7 @@ function Home() {
 
   return (
     <div className="flex flex-col gap-4 p-6">
-      <h1 className="text-4xl font-bold">TanStarter</h1>
-      <div className="flex items-center gap-2">
-        This is an unprotected page:
-        <pre className="rounded-md border bg-card p-1 text-card-foreground">
-          routes/index.tsx
-        </pre>
-      </div>
+      <h1 className="text-4xl font-bold">Mission72Aid</h1>
 
       {user ? (
         <div className="flex flex-col gap-2">
@@ -60,15 +54,6 @@ function Home() {
       )}
 
       <ThemeToggle />
-
-      <a
-        className="text-muted-foreground underline hover:text-foreground"
-        href="https://github.com/dotnize/tanstarter"
-        target="_blank"
-        rel="noreferrer noopener"
-      >
-        dotnize/tanstarter
-      </a>
     </div>
   );
 }
